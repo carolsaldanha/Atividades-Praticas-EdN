2cc3376e77b558042241aad421efb40fec48f03a
Caroline Costa Saldanha <125291901+carolsaldanha@users.noreply.github.com>
2025-06-18 20:52:46 -0300

    atividade 01

---
+++ Atividade 01/atv_03.py
@@ -0,0 +1,7 @@
+comprimemto = 12;
+largura = 14;
+altura = 20;
+
+volume = comprimemto * largura * altura;
+
+print(f"O volume é: {volume} cm³");
---
+++ Atividade 01/atv_04.py
@@ -0,0 +1,7 @@
+nomeProduto =  "Cadeira infantil";
+precoUnitario = 12.40
+quantidade = 3;
+
+total = precoUnitario * quantidade;
+
+print(f"O valor da compra é de R${total}");
